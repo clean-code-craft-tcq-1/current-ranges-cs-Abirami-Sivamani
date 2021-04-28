8c414260936933ae4a76378fc2fd50241a608d67
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-04-28 21:10:36 +0530

    Update BatteryCurrentMeasure.cs

--- BatteryChargeMeasure/BatteryCurrentMeasure.cs
+++ BatteryChargeMeasure/BatteryCurrentMeasure.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,25 @@ namespace BatteryChargeMeasure
 {
     public class BatteryCurrentMeasure
     {
+        public List<CurrentRangeWithCount> GetCurrentRangeWithCount(List<double> readings) 
+        {
+            List<CurrentRangeWithCount> _result = new List<CurrentRangeWithCount>();
+            if(!CurrentReadingValidation.IsReadingsEmpty(readings) && !CurrentReadingValidation.IsReadingsNaN(readings))
+            {
+                readings.Sort();
+                _result.Add(new CurrentRangeWithCount()
+                {
+                    ReadingCount = readings.Count,
+                    Range = (readings[0] + "-" + readings[readings.Count - 1]).ToString()
+                });
+            }        
+            return _result;
+        }
+    }
+
+    public class CurrentRangeWithCount
+    {
+        public string Range { get; set; }
+        public int ReadingCount { get; set; }
     }
 }
