a784b8e81fc6ea15a474af3bd1e1aceb96b5dd59
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-04-28 20:10:04 +0530

    Update BatteryCurrentMeasureTest.cs

--- BatteryChargeMeasureTest/BatteryCurrentMeasureTest.cs
+++ BatteryChargeMeasureTest/BatteryCurrentMeasureTest.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using BatteryChargeMeasure;
+using System.Collections.Generic;
 
 namespace BatteryChargeMeasure.Test
 {
@@ -7,8 +9,35 @@ namespace BatteryChargeMeasure.Test
     public class BatteryCurrentMeasureTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GivenEmptyListReadings_WhenNoReadings_ThenTrueIsReturned()
         {
+            CurrentReadingValidation _evaluate = new CurrentReadingValidation();
+            bool _readingsAvailable = _evaluate.IsReadingsEmpty(new List<double> {  });
+            Assert.IsTrue(_readingsAvailable);
+        }
+
+        [TestMethod]
+        public void GivenValidReadings_WhenListHasReadings_ThenFalseIsReturned()
+        {
+            CurrentReadingValidation _evaluate = new CurrentReadingValidation();
+            bool _readingsAvailable = _evaluate.IsReadingsEmpty(new List<double> { 2, 3, 4, 7, 8 });
+            Assert.IsFalse(_readingsAvailable);
+        }
+
+        [TestMethod]
+        public void GivenReadingsWithNaN_WhenReadingsHasNaN_ThenTrueIsReturned()
+        {
+            CurrentReadingValidation _evaluate = new CurrentReadingValidation();
+            bool _validReadings = _evaluate.IsReadingsNaN(new List<double> { 2, 3, Double.NaN, 6 });
+            Assert.IsTrue(_validReadings);
+        }
+
+        [TestMethod]
+        public void GivenValidReading_WhenListHasValidReadings_ThenFalseIsReturned()
+        {
+            CurrentReadingValidation _evaluate = new CurrentReadingValidation();
+            bool _validReadings = _evaluate.IsReadingsNaN(new List<double> { 2, 3, 3, 4 , 6 });
+            Assert.IsFalse(_validReadings);
         }
     }
 }
