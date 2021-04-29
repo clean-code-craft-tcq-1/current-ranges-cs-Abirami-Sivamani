2263538fcc636a92d5612f2d3710e09e6d603c67
Abirami <77603661+Abirami-Sivamani@users.noreply.github.com>
2021-04-29 19:42:30 +0530

    Update BatteryCurrentMeasureTest.cs

--- BatteryChargeMeasure.Test/BatteryCurrentMeasureTest.cs
+++ BatteryChargeMeasure.Test/BatteryCurrentMeasureTest.cs
@@ -11,35 +11,31 @@ namespace BatteryChargeMeasure.Test
         [TestMethod]
         public void GivenEmptyListReadings_WhenNoReadings_ThenTrueIsReturned()
         {
-            CurrentReadingValidation _evaluate = new CurrentReadingValidation();
-            bool _readingsAvailable = _evaluate.IsReadingsEmpty(new List<double> {  });
+            bool _readingsAvailable = CurrentReadingValidation.IsReadingsEmpty(new List<double> {  });
             Assert.IsTrue(_readingsAvailable);
         }
 
         [TestMethod]
         public void GivenValidReadings_WhenListHasReadings_ThenFalseIsReturned()
         {
-            CurrentReadingValidation _evaluate = new CurrentReadingValidation();
-            bool _readingsAvailable = _evaluate.IsReadingsEmpty(new List<double> { 2, 3, 4, 7, 8 });
+            bool _readingsAvailable = CurrentReadingValidation.IsReadingsEmpty(new List<double> { 2, 3, 4, 7, 8 });
             Assert.IsFalse(_readingsAvailable);
         }
 
         [TestMethod]
         public void GivenReadingsWithNaN_WhenReadingsHasNaN_ThenTrueIsReturned()
         {
-            CurrentReadingValidation _evaluate = new CurrentReadingValidation();
-            bool _validReadings = _evaluate.IsReadingsNaN(new List<double> { 2, 3, Double.NaN, 6 });
+            bool _validReadings = CurrentReadingValidation.IsReadingsNaN(new List<double> { 2, 3, Double.NaN, 6 });
             Assert.IsTrue(_validReadings);
         }
 
         [TestMethod]
         public void GivenValidReading_WhenListHasValidReadings_ThenFalseIsReturned()
         {
-            CurrentReadingValidation _evaluate = new CurrentReadingValidation();
-            bool _validReadings = _evaluate.IsReadingsNaN(new List<double> { 2, 3, 3, 4 , 6 });
+            bool _validReadings = CurrentReadingValidation.IsReadingsNaN(new List<double> { 2, 3, 3, 4 , 6 });
             Assert.IsFalse(_validReadings);
         }
-        
+
         [TestMethod]
         public void GivenSingleRangeReading_ReturnCurrentRangeWithCount()
         {
